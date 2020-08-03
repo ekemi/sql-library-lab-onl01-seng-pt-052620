3a5298e07307af594540cf63125ebb33f7135301
edmundo papi <edmundopierre@yahoo.com>
2020-08-03 14:31:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,12 @@ end
 
 def select_series_title_with_most_human_characters
   "Write your SQL query here"
-end
+  "SELECT series.title FROM series
+  LEFT OUTER JOIN characters
+  ON characters.series_id = series.id
+  WHERE characters.species ='human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species);
 
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
