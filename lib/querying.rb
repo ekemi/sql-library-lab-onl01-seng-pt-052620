69238fa960fa2cd865154077eb525abde0844036
edmundo papi <edmundopierre@yahoo.com>
2020-08-03 14:35:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,6 +40,7 @@ def select_series_title_with_most_human_characters
   GROUP BY series.title
   ORDER BY COUNT(characters.species ='human') DESC
   LIMIT 1;"
+end
 
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
